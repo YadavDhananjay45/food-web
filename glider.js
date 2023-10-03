c46e34cfac7c75862bc62397fe42bbd4acf26fe7
YadavDhanajay45 <dky99yadav@gmail.com>
2023-10-03 20:32:10 +0530

    updated third page

--- glider.js
+++ glider.js
@@ -1,26 +1,24 @@
 new Glider(document.querySelector(".glider"), {
-  // Mobile-first defaults
   slidesToShow: 1,
   slidesToScroll: 1,
   scrollLock: true,
   draggable: true,
   dragVelocity: 1.2,
+  rewind: true,
   dots: ".dots",
   responsive: [
     {
-      // screens greater than >= 775px
       breakpoint: 768,
       settings: {
-        // Set to `auto` and provide item width to adjust to viewport
         slidesToShow: 3,
         slidesToScroll: 1,
         scrollLock: true,
         draggable: true,
         dragVelocity: 1.5,
+        rewind: true,
       },
     },
     {
-      // screens greater than >= 1024px
       breakpoint: 1024,
       settings: {
         slidesToShow: 5,
@@ -28,6 +26,7 @@ new Glider(document.querySelector(".glider"), {
         scrollLock: true,
         draggable: true,
         dragVelocity: 1.5,
+        rewind: true,
       },
     },
   ],
