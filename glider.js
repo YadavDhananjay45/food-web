9db4b8c2e91ee584f89f9d1f7a43fc1552c045de
YadavDhanajay45 <dky99yadav@gmail.com>
2023-10-22 22:21:41 +0530

    added styles for medium screen

--- glider.js
+++ glider.js
@@ -10,18 +10,19 @@ new Glider(document.querySelector(".glider"), {
     {
       breakpoint: 768,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         scrollLock: true,
         draggable: true,
         dragVelocity: 1.5,
         rewind: true,
+        itemWidth: 200,
       },
     },
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 5,
+        slidesToShow: 3,
         slidesToScroll: 1,
         scrollLock: true,
         draggable: true,
